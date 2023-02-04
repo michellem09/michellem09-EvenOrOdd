a327665636f043e3c0a06c5412f57d22b0845c13
Michelle Mendez <mmendez5584@gmail.com>
2023-02-04 03:36:12 +0000

    Even or Odd Lab

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -14,6 +14,12 @@ public class Lab {
      * @return a String "Even" if n is even, and "Odd" of in is odd. The test cases are case-sensitive.
      */
     public String decide(int n){
-        return "";
+        int x = n % 2;
+
+        if(x == 0){
+            return "Even";
+        } else {
+            return "Odd";
+        }
     }
 }
